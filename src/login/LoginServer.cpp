42e457fcccce889c13b81c6a09483250cc4178d6
Eric Barr <apathy@swganh.org>
2009-07-02 04:21:28 +0000

    * src/login/LoginServer.cpp   src/zone/ZoneServer.cpp   (initializeProtocol): This method now adds appropriate   handlers to the protocol.

--- src/login/LoginServer.cpp
+++ src/login/LoginServer.cpp
@@ -7,6 +7,8 @@
 
 #include "LoginServer.h"
 #include "Logger.h"
+#include "SOEHandler.h"
+#include "LoginHandler.h"
 
 LoginServer::LoginServer(uint16_t port)
 : SocketServer(port)
@@ -19,4 +21,16 @@ void LoginServer::onUpdate()
 {}
 
 void LoginServer::initializeProtocol()
-{}
+{
+    protocol_.addHandler(1, std::tr1::bind(&HandleSessionRequest, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(3, std::tr1::bind(&HandleMultiPacket, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(5,  std::tr1::bind(&HandleDisconnect, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(6,  std::tr1::bind(&HandlePing, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(7, std::tr1::bind(&HandleNetStatus, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(9,  std::tr1::bind(&HandleDataChannel, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(21, std::tr1::bind(&HandleAcknowledge, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+
+    protocol_.addHandler(0x41131F96, std::tr1::bind(&HandleAuthentication, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+
+    protocol_.addHandler(0xD5899226, std::tr1::bind(&HandleSession, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+}
--- src/zone/ZoneServer.cpp
+++ src/zone/ZoneServer.cpp
@@ -8,7 +8,10 @@
 #include "ZoneServer.h"
 #include "Logger.h"
 #include "Session.h"
-
+#include "SOEHandler.h"
+#include "LoginHandler.h"
+#include "SpatialHandler.h"
+#include "ZoneInsertionHandler.h"
 
 enum ShuttleStates
 {
@@ -73,5 +76,33 @@ void ZoneServer::onUpdate()
 
 
 void ZoneServer::initializeProtocol()
-{}
+{
+    protocol_.addHandler(1, std::tr1::bind(&HandleSessionRequest, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(3, std::tr1::bind(&HandleMultiPacket, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(5, std::tr1::bind(&HandleDisconnect, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(6, std::tr1::bind(&HandlePing, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(7, std::tr1::bind(&HandleNetStatus, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(9, std::tr1::bind(&HandleDataChannel, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(21, std::tr1::bind(&HandleAcknowledge, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+
+    protocol_.addHandler(0x41131F96, std::tr1::bind(&HandleAuthentication, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+
+    protocol_.addHandler(0xD5899226, std::tr1::bind(&HandleSession, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+
+    protocol_.addHandler(0xB5098D76, std::tr1::bind(&HandleZoneInsertionRequest, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0x4C3D2CFA, std::tr1::bind(&HandleLoadTerrain, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0x43FD1C22, std::tr1::bind(&HandleLoadDone, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0xCA88FBAD, std::tr1::bind(&HandleClientReady, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0x48f493c5, std::tr1::bind(&HandleClientReady, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+
+		// Spatial
+    protocol_.addHandler(0x80ce5e46, std::tr1::bind(&HandleSpatial, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0xb719fa26, std::tr1::bind(&HandleSit, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0xa8a25c79, std::tr1::bind(&HandleStand, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0x7c8d63d4, std::tr1::bind(&HandleSpatialChat, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0xbd8d02af, std::tr1::bind(&HandleProne, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0x01b48b26, std::tr1::bind(&HandleKneel, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0x7759F35E, std::tr1::bind(&HandleMood, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+    protocol_.addHandler(0x32CF1BEE, std::tr1::bind(&HandleEmote, std::tr1::placeholders::_1, std::tr1::placeholders::_2));
+}
 
