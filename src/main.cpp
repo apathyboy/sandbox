497cb5ca32018628c804123dccf8ae69fc52edb4
Eric Barr <apathy@swganh.org>
2009-07-03 01:00:07 +0000

    * src/main.cpp: Normalized tabs to spaces.

--- src/main.cpp
+++ src/main.cpp
@@ -16,41 +16,36 @@
 #include "ZoneServer.h"
 #include "ProgramOptions.h"
 
-namespace po = boost::program_options;
-
 /* Program Constants */
 const char* config_filename = "openswg.cfg";
 
 // Run the OpenSWG Sandbox Server.
-int main(int argc, char *argv[])
-{      
-    // Load program options and print program info (if requested).
-    po::variables_map sandbox_options = buildProgramOptions(argc, argv, config_filename);
-   
+int main(int argc, char *argv[]) 
+{         
 	// Send server header output.
     std::cout << "OpenSWG Combat Upgrade Sandbox v2.0" << std::endl;
     std::cout << "Copyright (c) 2009 Spira Studios LLC" << std::endl << std::endl;
-    
+
+    // Load program options and print program info (if requested).
+    boost::program_options::variables_map sandbox_options = buildProgramOptions(argc, argv, config_filename);    
     Logger().log(INFO) << "Server starting with configuration file: " << config_filename;
 
 	// Begin the main execution phase. This begins by starting the server
 	// threads and then running until the exit command is received.
-	try {		
+    try {		
         LoginServer login_server(sandbox_options["login_port"].as<uint16_t>());
         boost::thread login_thread(std::tr1::bind(&LoginServer::run, &login_server));
 
         ZoneServer zone_server(sandbox_options["zone_port"].as<uint16_t>());
         boost::thread zone_thread(std::tr1::bind(&ZoneServer::run, &zone_server));
 
-		for(;;)
-		{
+        for(;;) {
             std::string cmd;
-			std::cin >> cmd;
+            std::cin >> cmd;
 
-			// Check for the exit command.
-			if (strcmp(cmd.c_str(), "exit") == 0 || strcmp(cmd.c_str(), "quit") == 0)
-			{
-				Logger().log(INFO) << "Shutting down the server";
+            // Check for the exit command.
+            if (strcmp(cmd.c_str(), "exit") == 0 || strcmp(cmd.c_str(), "quit") == 0) {
+                Logger().log(INFO) << "Shutting down the server";
                 
                 login_thread.interrupt();
                 login_thread.join();
@@ -58,14 +53,14 @@ int main(int argc, char *argv[])
                 zone_thread.interrupt();                
                 zone_thread.join();
 
-				break;
-			}
+                break;
+            }
 
             Logger().log(ERR) << "Command not found";
-		}
+        }
     } catch (std::exception& e) {
-		 Logger().log(ERR) << e.what();
-	}
+        Logger().log(ERR) << e.what();
+    }
 
-	return 0;
+    return 0;
 }
