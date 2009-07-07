2bdccb5efcf06ca9941f68b5c4e77f2524c15031
Eric Barr <apathy@swganh.org>
2009-07-07 13:45:39 +0000

    * include/UdpSocketListener.h,   src/shared/UdpSocketListener.cpp: New class, will replace   SocketServer.

---
+++ include/UdpSocketListener.h
@@ -0,0 +1,50 @@
+/*
+ * OpenSWG: GalaxyEngine - UdpSocketListener.h
+ *
+ * @copyright   Copyright (c) 2009 Eric S. Barr Jr.
+ * @author      Eric S. Barr Jr. <eric.barr@ericscottbarr.com>
+**/
+
+#ifndef UDP_SOCKET_LISTENER_H_
+#define UDP_SOCKET_LISTENER_H_
+
+#ifdef _MSC_VER
+#include "stdint.h"
+#else
+#include <cstdint>
+#endif
+
+#include <tr1/functional>
+#include <tr1/memory>
+
+#include "NetworkAddress.h"
+
+class ByteBuffer;
+
+class UdpSocketListener
+{
+public:
+    typedef std::tr1::function<void (const NetworkAddress&, std::tr1::shared_ptr<ByteBuffer>)> Callback;
+
+    UdpSocketListener(uint16_t port);
+    ~UdpSocketListener();
+
+    uint16_t port();
+    void     port(uint16_t port);
+
+    Callback callback();
+    void     callback(Callback callback);
+
+    void poll();
+
+private:
+    UdpSocketListener();
+    UdpSocketListener(const UdpSocketListener&);
+    UdpSocketListener& operator=(const UdpSocketListener&);
+
+    class UdpSocketListenerImpl;
+    std::tr1::shared_ptr<UdpSocketListenerImpl> impl_;
+};
+
+#endif // UDP_SOCKET_LISTENER_H_
+
---
+++ src/shared/UdpSocketListener.cpp
@@ -0,0 +1,125 @@
+/*
+ * OpenSWG: GalaxyEngine - UdpSocketListener.cpp
+ *
+ * @copyright   Copyright (c) 2009 Eric S. Barr Jr.
+ * @author      Eric S. Barr Jr. <eric.barr@ericscottbarr.com>
+**/
+
+#include "boost/asio.hpp"
+#include "UdpSocketListener.h"
+#include "ByteBuffer.h"
+
+
+/** UdpSocketListener::UdpSocketListenerImpl **/ 
+class UdpSocketListener::UdpSocketListenerImpl
+{
+public:
+    UdpSocketListenerImpl(uint16_t port)
+        : io_service_()
+        , socket_(io_service_, boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port))
+        , port_(port)
+    {}
+
+    uint16_t port()
+    {
+        return port_;
+    }
+
+    void port(uint16_t port)
+    {
+        socket_.bind(boost::asio::ip::udp::endpoint(boost::asio::ip::udp::v4(), port));
+        port_ = port;
+    }
+
+    Callback callback()
+    {
+        return callback_;
+    }
+
+    void callback(Callback callback)
+    {
+        callback_ = callback;
+    }
+
+    void poll()
+    {
+        io_service_.poll();
+    }
+
+private:
+    UdpSocketListenerImpl(const UdpSocketListenerImpl&);
+    UdpSocketListenerImpl& operator=(const UdpSocketListenerImpl&);
+  
+    void handleReceive(const boost::system::error_code& error, size_t bytes_received)
+    {  	
+        std::tr1::shared_ptr<ByteBuffer> message(new ByteBuffer(&buffer_[0], bytes_received));
+        callback_(remote_endpoint_, message);
+
+  	    asyncRecieve();
+    }
+
+    void asyncRecieve()
+    {
+  	    socket_.async_receive_from(
+            boost::asio::buffer(buffer_),
+  		    remote_endpoint_,
+            std::tr1::bind(
+                &UdpSocketListenerImpl::handleReceive, 
+                this, 
+                std::tr1::placeholders::_1, 
+                std::tr1::placeholders::_2
+            )
+        );  
+    }
+
+    boost::asio::io_service         io_service_;
+    boost::asio::ip::udp::socket    socket_;
+    boost::asio::ip::udp::endpoint  remote_endpoint_;
+    
+    UdpSocketListener::Callback     callback_;
+
+    std::vector<uint8_t>            buffer_;
+
+    uint16_t port_;
+};
+
+
+/** UdpSocketListener **/
+
+UdpSocketListener::UdpSocketListener(uint16_t port)
+    : impl_(new UdpSocketListener::UdpSocketListenerImpl(port))
+{}
+
+
+UdpSocketListener::~UdpSocketListener()
+{}
+    
+
+uint16_t UdpSocketListener::port()
+{
+    return impl_->port();
+}
+
+
+void UdpSocketListener::port(uint16_t port)
+{
+    impl_->port(port);
+}
+
+
+UdpSocketListener::Callback UdpSocketListener::callback()
+{
+    return impl_->callback();
+}
+    
+void UdpSocketListener::callback(UdpSocketListener::Callback callback)
+{
+    impl_->callback(callback);
+}
+
+
+void UdpSocketListener::poll()
+{
+    impl_->poll();
+}
+
